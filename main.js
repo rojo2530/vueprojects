a08c4b29ce6e58f63849b52a0704d7b19bde2997
Jose <jose@webempresa.eu>
2018-02-03 23:32:07 +0100

    Primer commit

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+
+const dictionary = {
+    en: {
+        messages: {
+            confirmed: function () {
+                return "Your password is not confirmed"
+            },
+            email: function () {
+                return "I really dont like your email"
+            }
+        }
+    }
+};
+
+Vue.use(VeeValidate,{
+    fieldsBagName: 'vFields',
+    locale:'en',
+    dictionary: {
+        es: {
+            custom: {
+                email: {
+                    required: 'Your email is empty' // messages can be strings as well.
+                },
+                name: {
+                    required: 'Your name is empty'
+                }
+            }
+        }
+    }
+});
+
 var app = new Vue({
     el: ".container",
     data: {
